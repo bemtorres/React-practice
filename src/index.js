1b2e6081fb933eb153c8bbca97ace3b490a08bd2
Benjamin <benja.mora.torres@gmail.com>
2018-02-28 21:20:00 -0300

    logear con google

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDBkjE8MyuMMfa2vO7rDOmn8T7hhihcaL4",
+    authDomain: "photoapp-504b0.firebaseapp.com",
+    databaseURL: "https://photoapp-504b0.firebaseio.com",
+    projectId: "photoapp-504b0",
+    storageBucket: "photoapp-504b0.appspot.com",
+    messagingSenderId: "373727940221"
+  });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
